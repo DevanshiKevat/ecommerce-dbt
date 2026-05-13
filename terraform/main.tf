31cbe6193bd54c40c5cc32d5c1fc4e6dfa7c0162
Devanshi Kevat <135352265+DevanshiKevat@users.noreply.github.com>
2026-05-13 14:29:26 +0530

    Creare main.tf file

---
+++ terraform/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source = "Snowflake-Labs/snowflake"
+      version = "~> 0.98"
+    }
+  }
+}
+
+provider "snowflake" {
+  account  = var.snowflake_account
+  user     = var.snowflake_user
+  password = var.snowflake_password
+  role     = "TERRAFORM_ROLE"
+}
